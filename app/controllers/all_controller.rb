f8cdffe77d65096b8e6b42535be59284f59bbac3
Michelle Ling <michelleling225@gmail.com>
2017-10-11 14:39:35 -0700

    Finished hw4 yayayayyay thanks jason and ken

---
+++ app/controllers/all_controller.rb
@@ -0,0 +1,10 @@
+class AllController < ApplicationController
+	def all
+		@cats = Cat.all
+		@todos = Todo.all
+		@users = User.all
+		render "all"
+
+	end
+
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(name: 'ChicKen')
+User.create(username: 'Ken', email: 'ken.chen@email.com', age: 'unknown')
+Todo.create(todos: 'everything', finished: false)
